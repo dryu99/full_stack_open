d61227a98b44db941ee2fc042e0775f636495f29
Daniel Ryu <daniel.ryu@aimsio.com>
2020-04-12 19:35:40 -0700

    complete exercise 1.9 (1.8 was unknowingly completed earlier)

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -48,33 +48,44 @@ const Feedback = ({ stats }) => {
 }
 
 const Statistics = ({ stats }) => {
-  // calculate total count
-  const totalCount = stats.reduce(
-    (acc, currentStat) => acc + currentStat.count,
-    0
-  );
+  if (stats.some(stat => stat.count > 0)) {
+    // calculate total count
+    const totalCount = stats.reduce(
+      (acc, currentStat) => acc + currentStat.count,
+      0
+    );
+    
+    // calculate average
+    const goodScore = stats[0].count * 1;
+    const neutralScore = stats[1].count * 0;
+    const badScore = stats[2].count * -1;
+    const average = totalCount !== 0 ? (goodScore + neutralScore + badScore) / totalCount : 0;
+    
+    // calculate positive %
+    const positiveFraction = totalCount !== 0 ? stats[0].count / totalCount : 0;
+    const positivePercentage = (positiveFraction * 100) + ' %';
+    
+    return (
+      <div>
+        <Header text={'Statistics'} />
+        <Stat text={stats[0].text} value={stats[0].count} />
+        <Stat text={stats[1].text} value={stats[1].count} />
+        <Stat text={stats[2].text} value={stats[2].count} />
+        <Stat text={"total"} value={totalCount}/>
+        <Stat text={"average"} value={average}/>
+        <Stat text={"positive"} value={positivePercentage}/>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Header text={'Statistics'} />
+        <p>No feedback given</p>
+      </div>
+    );
+  }
 
-  // calculate average
-  const goodScore = stats[0].count * 1;
-  const neutralScore = stats[1].count * 0;
-  const badScore = stats[2].count * -1;
-  const average = totalCount !== 0 ? (goodScore + neutralScore + badScore) / totalCount : 0;
-
-  // calculate positive %
-  const positiveFraction = totalCount !== 0 ? stats[0].count / totalCount : 0;
-  const positivePercentage = (positiveFraction * 100) + ' %'
-
-  return (
-    <div>
-      <Header text={'Statistics'} />
-      <Stat text={stats[0].text} value={stats[0].count} />
-      <Stat text={stats[1].text} value={stats[1].count} />
-      <Stat text={stats[2].text} value={stats[2].count} />
-      <Stat text={"total"} value={totalCount}/>
-      <Stat text={"average"} value={average}/>
-      <Stat text={"positive"} value={positivePercentage}/>
-    </div>
-  )
+  return statsComponent;
 }
 
 const Stat = ({ text, value }) => {
