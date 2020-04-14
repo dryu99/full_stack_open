a7d781007e1e8a11969dcbd8fe7e2e1baa282a16
Daniel Ryu <daniel.ryu@aimsio.com>
2020-04-14 02:43:39 -0700

    complete 'course contents' exercise 2.1

---
+++ part_2/course_contents/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Course from "./components/Course"
+
+const App = ({ course }) => {
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+}
+
+export default App;
