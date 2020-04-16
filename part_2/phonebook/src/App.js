bc79f0fc4e74097e94e67fd132df3cb0f82aa4b2
Daniel Ryu <daniel.ryu@aimsio.com>
2020-04-15 21:02:36 -0700

    complete 'phonebook' exercise 2.6

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Person from './components/Person';
 
 const App = () => {
   const [ persons, setPersons] = useState([
@@ -6,19 +7,41 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    var newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name:
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+          <Person key={person.name} person={person}/>
+        )}
+      </ul>
     </div>
   )
 }
