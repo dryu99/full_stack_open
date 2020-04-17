26dcd71a31b27b3d80a53675740d90c19ee616b1
Daniel Ryu <daniel.ryu@aimsio.com>
2020-04-17 01:19:05 -0700

    complete 'phonebook' exercise 2.11

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filterName, setFilterName ] = useState('');
+
+  const hook = () => {
+    console.log('hooking');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promsie fulfilled!')
+        setPersons(response.data);
+        setFilteredPersons(response.data);
+      });
+  }
+
+  useEffect(hook, []);
+  console.log('persons length: ', persons.length);
 
   const handleFilterNameChange = (event) => {
     const newFilterName = event.target.value;
