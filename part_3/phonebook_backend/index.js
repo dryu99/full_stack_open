8c4664fb804d674164f23870eff9c4072a619cff
Daniel Ryu <rujjiryu99@gmail.com>
2020-04-24 02:01:31 -0700

    complete 'phonebook' exercise 3.3-3.6

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
       "name": "Arto Hellas",
@@ -24,13 +26,59 @@ let persons = [
     }
 ];
 
-app.get('/', (req, res) => {
-  res.send('<p>You should probably go to http://localhost:3001/api/persons</p>')
-});
-
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  // error handling
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    });
+  } else {
+    const person = persons.find(person => person.name === body.name);
+    if (person) {
+      return res.status(400).json({
+        error: 'person already exists'
+      });
+    }
+  }
+
+  const newPerson = {
+    ...body,
+    id: generateId()
+  }
+
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
+const generateId = () => {
+  console.log();
+  const maxId = persons.reduce((acc, currentPerson) => Math.max(acc, currentPerson.id), 0);
+  const offset = Math.ceil(Math.random() * 10) // random num between 1 - 10
+  return maxId + offset;
+}
 
 app.get('/info', (req, res) => {
   const infoHtml = `<p>Phonebook has info for ${persons.length} people</p>`
